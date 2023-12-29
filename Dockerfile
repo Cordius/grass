b2708ab287bfa6863c2661bdb2d978951c77c9be
Wu Zongyong <wuzongyong@cordius.cn>
2023-12-29 13:54:35 +0800

    use playwright to support arm
    Signed-off-by: Wu Zongyong <wuzongyong@cordius.cn>

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,10 @@
-FROM python:3-alpine
-RUN sed -i 's/dl-cdn.alpinelinux.org/mirrors.aliyun.com/g' /etc/apk/repositories
-RUN apk add --no-cache chromium chromium-chromedriver unzip
+FROM python:3-slim
+RUN apt install --no-cache chromium chromium-chromedriver unzip
 #RUN ln -s /usr/bin/chromium-browser /usr/bin/chromium
 
 WORKDIR /usr/src/app
 COPY src .
-RUN pip install --no-cache-dir -r ./requirements.txt -i https://mirrors.aliyun.com/pypi/simple/
+RUN pip install --no-cache-dir -r ./requirements.txt 
 
 CMD [ "python", "./main.py" ]
 EXPOSE 80
--- src/main.py
+++ src/main.py
@@ -1,109 +1,103 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.chrome.service import Service as ChromiumService
-
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.os_manager import ChromeType
+import json
 import time
 import requests
 import os
 import re
+import zipfile
 from flask import Flask
+from playwright.sync_api import sync_playwright
 
 extensionId = 'ilehaonighjijnmpnagapkhpcdbhclfg'
-CRX_URL = "https://clients2.google.com/service/update2/crx?response=redirect&prodversion=98.0.4758.102&acceptformat=crx2,crx3&x=id%3D~~~~%26uc&nacl_arch=x86-64"
-USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36"
+extensionName = 'grass.zip'
+unpackedExtensionName = 'extension'
+CRX_URL = "https://clients2.google.com/service/update2/crx?response=redirect&prodversion=98.0.4758.102&acceptformat=crx2,crx3&x=id%3D~~~~%26uc"
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36"
 
 USER = os.environ['GRASS_USER']
 PASSW = os.environ['GRASS_PASS']
 
+if not USER or not PASSW:
+    print('Please set GRASS_USER and GRASS_PASS env variables')
+    exit()
 
-#https://gist.github.com/ckuethe/fb6d972ecfc590c2f9b8
+app = Flask(__name__)
+
 def download_extension(extension_id):
     url = CRX_URL.replace("~~~~", extension_id)
     headers = {"User-Agent": USER_AGENT}
     r = requests.get(url, stream=True, headers=headers)
-    with open("grass.crx", "wb") as fd:
+    with open(extensionName, "wb") as fd:
         for chunk in r.iter_content(chunk_size=128):
             fd.write(chunk)
 
-if os.path.exists("grass.crx"):
-    print("Extension grass already exists, skip download...")
-else:
-    print('Downloading extension...')
-    download_extension(extensionId)
-    print('Downloaded! Installing extension and driver manager...')
+def fixup_extension_manifest(extension_path):
+    manifest_file = os.path.join(extension_path, "manifest.json")
+    with open(manifest_file, 'r') as file:
+        data = json.load(file)
 
-# are they set?
-if USER == '' or PASSW == '':
-    print('Please set GRASS_USER and GRASS_PASS env variables')
-    exit()
+    data['manifest_version'] = 2
 
-options = webdriver.ChromeOptions()
-#options.binary_location = '/usr/bin/chromium-browser'
-options.add_argument("--headless=new")
-options.add_argument("--disable-dev-shm-usage")
-options.add_argument('--no-sandbox')
+    with open(manifest_file, 'w') as file:
+        json.dump(data, file, indent=4)
 
-options.add_extension('grass.crx')
 
-print('Installed! Starting...')
-driver = webdriver.Chrome(options=options)
+def setup_browser():
+    with sync_playwright() as p:
+        if os.path.exists(extensionName):
+            print('Extension already exists, skip download...')
+        else:
+            print('Downloading extension...')
+            download_extension(extensionId)
+            print('Extension downloaded!')
+        
+        # unpack extension
+        with zipfile.ZipFile(extensionName, 'r') as zip_ref:
+            zip_ref.extractall(unpackedExtensionName)
+        print('Extension unpacked!')
+        
+        # Launch a Chromium browser with the extension
+        extension_path = os.path.join(os.getcwd(), unpackedExtensionName)
+        fixup_extension_manifest(extension_path)
+        context = p.chromium.launch_persistent_context(os.getcwd(), headless=False,
+            bypass_csp=True,
+            args=[
+                '--headless=new',
+                f'--disable-extensions-except={extension_path}',
+                f'--load-extension={extension_path}',
+        ])
+        background = context.service_workers[0]
+        if not background:
+            background = context.wait_for_event("serviceworker")
+        print('Browser launched!')
+        page = context.new_page()
 
-driver.get('chrome-extension://'+extensionId+'/index.html')
-print('Started! Logging in...')
-time.sleep(3)
+        # The URL of the extension's page may vary depending on the extension ID and its internal structure
+        page.goto(f'chrome-extension://{extensionId}/index.html')
 
-sleep = 0
-while True:
-    try:
-        driver.find_element('xpath', '//*[@name="user"]')
-        driver.find_element('xpath', '//*[@name="password"]')
-        driver.find_element('xpath', '//*[@type="submit"]')
-        break
-    except:
-        time.sleep(3)
-        print('Loading login form...')
-        sleep += 1
-        if sleep > 30:
-            driver.quit()
-            exit()
+        # Log in to the extension
+        page.get_by_placeholder('Username or Email').fill(USER)
+        page.get_by_placeholder('Password').fill(PASSW)
+        page.click('button[type="submit"]')
+        print('Logged in! Waiting for connection...')
+        time.sleep(2)
 
-#find name="user"
-user = driver.find_element('xpath', '//*[@name="user"]')
-passw = driver.find_element('xpath', '//*[@name="password"]')
-submit = driver.find_element('xpath', '//*[@type="submit"]')
+        # Wait for the network quality element to be visible
+        print(page.content())
+        page.get_by_text('Network quality').wait_for()
+        print('Connected!')
+        return page
 
-#get user from env
-user.send_keys(USER)
-passw.send_keys(PASSW)
-submit.click()
+page = setup_browser()
 
-print('Logged in! Waiting for connection...')
-sleep = 0
-while True:
-    try:
-        driver.find_element('xpath', '//*[contains(text(), "Network quality")]')
-        break
-    except:
-        time.sleep(3)
-        print('Loading connection...')
-        sleep += 1
-        if sleep > 30:
-            driver.quit()
-            exit()
-
-print('Connected! Starting API...')
-#flask api
-app = Flask(__name__)
 @app.route('/')
 def get():
-    network_quality = driver.find_element('xpath', '//*[contains(text(), "Network quality")]').text
-    network_quality = re.findall(r'\d+', network_quality)[0]
-    lifetime_earnings = driver.find_element('xpath', '//*[contains(text(), "Lifetime earnings")]')
-    lifetime_earnings = lifetime_earnings.find_element('xpath','following-sibling::div')
-    lifetime_earnings = lifetime_earnings.find_element('xpath','child::p').text
-    return {'network_quality': network_quality, 'lifetime_earnings': lifetime_earnings}
+    # Retrieve the network quality and lifetime earnings
+    # specific_text = 'Network quality'
+    # network_quality = page.inner_text(f"text='{specific_text}'")
+    # network_quality = re.findall(r'\d+', network_quality)[0]
+    # lifetime_earnings = page.inner_text('//*[contains(text(), "Lifetime earnings")]//*[following-sibling::div]//child::p')
+
+    # return {'network_quality': network_quality, 'lifetime_earnings': lifetime_earnings}
+    return page.content()
 
 app.run(host='0.0.0.0',port=80, debug=False)
-driver.quit()
